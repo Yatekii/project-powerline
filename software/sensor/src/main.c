3f6f4f130ee66f4479daa85be4c56a789fcfa172
Noah Huesser <yatekii@yatekii.ch>
2016-04-11 17:18:01 +0200

    Added sample code for GPIO. Doesn't work yet because of a hardware bug

--- software/sensor/src/main.c
+++ software/sensor/src/main.c
@@ -1,13 +1,29 @@
 #include <asf.h>
 #include <led.h>
 
+#define BTN PIN_PA16
+
+int leds_active = 1;
+
 void SysTick_Handler(void){
-	toggle_led(LED_RED);
-	toggle_led(LED_GREEN);
+	if(leds_active){
+		toggle_led(LED_RED);
+		toggle_led(LED_GREEN);
+	}
+	else{
+		set_led(LED_RED, LED_STATE_INACTIVE);
+		set_led(LED_GREEN, LED_STATE_INACTIVE);
+	}
 }
 
 void main(void){
 	system_init();
+
+	struct port_config pin_conf;
+	port_get_config_defaults(&pin_conf);
+
+	pin_conf.direction  = PORT_PIN_DIR_INPUT;
+	port_pin_set_config(BTN, &pin_conf);
 
 	config_led(LED_RED);
 	config_led(LED_GREEN);
@@ -17,5 +33,12 @@ void main(void){
 	SysTick_Config(system_gclk_gen_get_hz(GCLK_GENERATOR_0) / 8);
 
 	while (1){
+		if(port_pin_get_input_level(BTN) == 0 && leds_active){
+			leds_active = 0;
+		}
+		else if(port_pin_get_input_level(BTN) == 1 && leds_active == 0){
+			leds_active = 1;
+			set_led(LED_GREEN, LED_STATE_ACTIVE);
+		}
 	}
 }

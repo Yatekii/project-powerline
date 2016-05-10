842471f39542e107c35696680b09168b29e798b6
retonussbaumer <reto.nussbaumer@students.fhnw.ch>
2016-05-10 10:48:30 +0200

    bla bla

--- software/supervisor/GUI.py
+++ software/supervisor/GUI.py
@@ -28,6 +28,7 @@ class Example(QWidget):
 
         hystoryBt = QPushButton("Hystory")
         settingBt = QPushButton("Einstellungen")
+        strom = 
 
         hbox = QHBoxLayout()
         hbox.addWidget(hystoryBt)
@@ -38,50 +39,14 @@ class Example(QWidget):
         vbox.addStretch(1)
 
 
+
         self.setLayout(vbox)
 
         self.setGeometry(100,100,320,240)
         self.setWindowTitle('Home')
         self.show()
 
-    def initUI2(self):
-        okButton = QPushButton("OK")
-        cancelButton = QPushButton("Cancel")
 
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(okButton)
-        hbox.addWidget(cancelButton)
-
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-
-        self.setLayout(vbox)
-
-        self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle('Buttons')
-        self.show()
-
-
-        names = ['Cls', 'Bck', '', 'Close',
-                 '7', '8', '9', '/',
-                '4', '5', '6', '*',
-                 '1', '2', '3', '-',
-                '0', '.', '=', '+']
-
-        positions = [(i,j) for i in range(5) for j in range(4)]
-
-        for position, name in zip(positions, names):
-
-            if name == '':
-                continue
-            button = QPushButton(name)
-            grid.addWidget(button, *position)
-
-        self.move(300, 150)
-        self.setWindowTitle('Calculator')
-        self.show()
 
 if __name__ == '__main__':
 

7d65e97909489c199077a49b28695583ae6abc82
marcelheymann <marcel.heymann@students.fhnw.ch>
2016-06-14 17:54:58 +0200

    Just some random commmiting

--- software/supervisor/evaluator.py
+++ software/supervisor/evaluator.py
@@ -1,12 +1,28 @@
-# todo imports
+    #imports
+    from database import session, Strings, strings, String1, string1, String2, string2, String3, string3, Panels, panels
+    import time, math
 
-# todo wird vermutlich per Interrupt angekickt
+    string_1 = session.query(String1).all()
+    string_2 = session.query(String2).all()
+    string_3 = session.query(String3).all()
 
 
-# todo Die erhaltenen "verdächtigen" Module mit deren History vergleichen
-
-# todo Die erhaltenen "verdächtigen" Strings mit deren History vergleichen
-
-# todo Meldebedingungen prüfen (2 von 3 flags gesetzt oder whatever)
-
-# todo Falls nötig Melder Callback ankicken
+    # moduleitems: list with all references of one module-item of the last 24 hours
+    # deviations: list with all the deviations of 24 hours of one module
+    for item in string_1:
+        moduleitems = session.query(Panels).filter((panels.c.serialnumber == item.serialnumber) & (panels.c.timestamp == (time.time()-24*60*60))).all()
+        deviations = []
+        for module in moduleitems:
+            deviations.append(module.deviation)
+        average = ((sum(deviations))/(len(deviations)))
+        result = []
+        for deviation in deviations:
+            result.append((deviation-average)^2)
+        st_deviation = math.sqrt(((sum(result))/(len(result)-1)))
+        not_good = []
+        good = []
+        for deviation in deviations:
+            if deviation > st_deviation:
+                not_good.append(deviation)
+            else:
+                good.append(deviation)
--- software/supervisor/reporter.py
+++ software/supervisor/reporter.py
@@ -1,13 +1,8 @@
 # todo imports
 
-# todo wird vermutlich per Interrupt angekickt
+# todo GUI ankicken um Meldung auf Display darstellen
 
-
-# todo Interrupt in configurator auslösen und Meldung auf Display darstellen
-
-# todo Relaiskontakt auslösen (umsetzung je nach additional board)
+# todo Relaiskontakt auslösen
 
 # todo SMS-Nachricht auslösen
 
-
-# todo Interrupt
